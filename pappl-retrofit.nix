9f4265fa637b45dfa2e4e2e797c452d4bd834c33
Blake Haug <blake@blakehaug.com>
2026-06-03 14:16:24 -0700

    Update pappl-retrofit to latest master commit

--- pappl-retrofit.nix
+++ pappl-retrofit.nix
@@ -16,8 +16,8 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "OpenPrinting";
     repo = "pappl-retrofit";
-    rev = "1.0b2";
-    hash = "sha256-YBU1uFleyDsseHnEnbEd4XFL/4NF2WTMK3kNDZjyBaY=";
+    rev = "edca0c166920230f55f6c34553ee0d1c7a6319a8";
+    hash = "sha256-3rSxlD3UjSVUhXuxgRu8J+we4VpXbhiqKP89zDW5kE0=";
   };
 
   nativeBuildInputs = [
